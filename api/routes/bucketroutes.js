f83458b8875143a660de34a01286a84c64afddbc
Christopher Riffle <cmr629@gmail.com>
2019-06-23 15:46:34 -0400

    modified endpoints

--- api/routes/bucketroutes.js
+++ api/routes/bucketroutes.js
@@ -5,12 +5,12 @@ const express = require('express');
 
 const router = express.Router(); 
 
-router.get('/bucketitem/:id', (req, res) => {
+router.get('/item/:id', (req, res) => {
   
     bucketDb.getBucketItemById(req.params.id)
     .then(response => {
         if (response)
-            res.status(200).json({bucketitem: response});
+            res.status(200).json({item: response});
         else
             res.status(404).json({ message: "Bucket item not found" });
     })
@@ -19,7 +19,7 @@ router.get('/bucketitem/:id', (req, res) => {
     });
 });
 
-router.post('/bucketitem', (req, res) => {
+router.post('/item', (req, res) => {
   
     bucketDb.createBucketItem(req.body)
     .then(response => {
@@ -30,7 +30,7 @@ router.post('/bucketitem', (req, res) => {
     });
 });
 
-router.put('/bucketitem/:id', (req, res) => {
+router.put('/item/:id', (req, res) => {
   
     bucketDb.updateBucketItem(req.params.id, req.body)
     .then(response => {
@@ -41,7 +41,7 @@ router.put('/bucketitem/:id', (req, res) => {
     });
 });
 
-router.delete('/bucketitem/:id', (req, res) => {
+router.delete('/item/:id', (req, res) => {
   
     bucketDb.deleteBucketItem(req.params.id)
     .then(response => {
@@ -52,7 +52,7 @@ router.delete('/bucketitem/:id', (req, res) => {
     });
 });
 
-router.get('/bucketitem/:item_id/posts', (req, res) => {
+router.get('/item/:item_id/posts', (req, res) => {
   
     bucketPostDb.getBucketItemPostByItemId(req.params.item_id)
     .then(response => {
@@ -66,12 +66,12 @@ router.get('/bucketitem/:item_id/posts', (req, res) => {
     });
 });
 
-router.get('/bucketitem/post/:id', (req, res) => {
+router.get('/item/post/:id', (req, res) => {
   
     bucketPostDb.getBucketItemPostById(req.params.id)
     .then(response => {
         if (response)
-            res.status(200).json({bucketitempost: response});
+            res.status(200).json({post: response});
         else
             res.status(404).json({ message: "Post not found" });
     })
@@ -80,7 +80,7 @@ router.get('/bucketitem/post/:id', (req, res) => {
     });
 });
 
-router.post('/bucketitem/post', (req, res) => {
+router.post('/item/post', (req, res) => {
   
     bucketPostDb.createBucketItemPost(req.body)
     .then(response => {
@@ -91,7 +91,7 @@ router.post('/bucketitem/post', (req, res) => {
     });
 });
 
-router.put('/bucketitem/post/:id', (req, res) => {
+router.put('/item/post/:id', (req, res) => {
   
     bucketPostDb.updateBucketItemPost(req.params.id, req.body)
     .then(response => {
@@ -102,7 +102,7 @@ router.put('/bucketitem/post/:id', (req, res) => {
     });
 });
 
-router.delete('/bucketitem/post/:id', (req, res) => {
+router.delete('/item/post/:id', (req, res) => {
   
     bucketPostDb.deleteBucketItemPost(req.params.id)
     .then(response => {
@@ -113,12 +113,12 @@ router.delete('/bucketitem/post/:id', (req, res) => {
     });
 });
 
-router.get('/bucketitem/post/:post_id/images', (req, res) => {
+router.get('/item/post/:post_id/images', (req, res) => {
   
     bucketPostImageDb.getBucketItemPostByPostId(req.params.post_id)
     .then(response => {
         if (response)
-            res.status(200).json({posts: response});
+            res.status(200).json({images: response});
         else
             res.status(404).json({ message: "No images for this post found" });
     })
@@ -127,12 +127,12 @@ router.get('/bucketitem/post/:post_id/images', (req, res) => {
     });
 });
 
-router.get('/bucketitem/post/image/:id', (req, res) => {
+router.get('/item/post/image/:id', (req, res) => {
   
     bucketPostImageDb.getBucketItemPostImageById(req.params.id)
     .then(response => {
         if (response)
-            res.status(200).json({bucketitempost: response});
+            res.status(200).json({image: response});
         else
             res.status(404).json({ message: "Image not found" });
     })
@@ -141,7 +141,7 @@ router.get('/bucketitem/post/image/:id', (req, res) => {
     });
 });
 
-router.post('/bucketitem/post/image', (req, res) => {
+router.post('/item/post/image', (req, res) => {
   
     bucketPostImageDb.createBucketItemPostImage(req.body)
     .then(response => {
@@ -152,7 +152,7 @@ router.post('/bucketitem/post/image', (req, res) => {
     });
 });
 
-router.put('/bucketitem/post/image/:id', (req, res) => {
+router.put('/item/post/image/:id', (req, res) => {
   
     bucketPostImageDb.updateBucketItemPostImage(req.params.id, req.body)
     .then(response => {
@@ -163,7 +163,7 @@ router.put('/bucketitem/post/image/:id', (req, res) => {
     });
 });
 
-router.delete('/bucketitem/post//image/:id', (req, res) => {
+router.delete('/item/post//image/:id', (req, res) => {
   
     bucketPostImageDb.deleteBucketItemPostImage(req.params.id)
     .then(response => {
